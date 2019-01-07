8049373d751e6d05298ef55447b730cebc8804eb
Rachel Hofer <rhofer3@gmail.com>
2019-01-07 18:46:42 -0500

    scope

---
+++ scopeAndConditions/index.js
@@ -0,0 +1,44 @@
+// Ex. Function Declaration
+function foo() {
+  console.log("Function foo is called");
+}
+foo()
+
+
+// Ex. of Function Expression (First class function in JS)
+var bar = function() {
+  console.log("Function bar is called");
+}
+bar()
+
+// Scope: a scope dictates a portion of the program where a particular 
+// variable is accessible. If the variable is accessed outside of the scope, 
+// the variable does not exist. 
+//Scopes have a hierarchy, a scope inside of a scope.
+
+// ex. of BLOCK scope in languages like Java or C++ NOT in Javascript
+// {
+// var a = "This is a simple way to create a scope" 
+// }
+
+// ex. of BLOCK scope in Javascript: does not perform the same way as above like in Java or C++
+var name = "Rachel";
+
+if(name === "Rachel"){
+  var department = "Engineering"
+}
+
+console.log(name) // === Rachel
+console.log(department) // === Engineering (JS is not a BLOCK scope, it is functional scope)
+
+// ex. of Functional Scope
+var name = "Rachel";
+
+function allocateDepartment(){
+  if(name === "Rachel"){
+  var department = "Engineering"
+}
+}
+
+console.log(name) // === Rachel
+console.log(department) // === ERROR: department is declared INSIDE the function, therefore not accessible outside of that function.
