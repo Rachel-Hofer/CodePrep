4a8f64dbfb5f349f6f4a1f2588b9d84038690ea7
Rachel Hofer <rhofer3@gmail.com>
2019-01-07 19:59:07 -0500

    global objects

---
+++ globalObjects/index.js
@@ -0,0 +1,11 @@
+// Global Object: like the root object which holds all global variables. It depends on which runtime you are using.
+
+// When you run JS in the Browser, the Global Object is USUALLY the window
+
+//ex:
+var abc = 100; // global variable
+var def = "Hello Window"; // global variable
+
+// When you create a Global Variable, you are actually creating a property on the Global Object (window)
+
+// Whatever you do in the Global Scope is basically jsut creating properties on the Global Object. (In the case of a browser, Global Scope will always be the window)
