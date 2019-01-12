c4231d2c75d306ee07a352faf764ded05f45fc4a
Rachel Hofer <rhofer3@gmail.com>
2019-01-12 08:07:57 -0500

    type coercion

---
+++ typeCoercion/index.js
@@ -0,0 +1,8 @@
+// Type Coercion: you have variables declared with an assigned value, you are operating on those variables, and there are certain times were the interpreter or compiler will have to do some automatic type conversions for you in order to make that operation work.
+  // Example: 123 + "4"
+  // Answer: "1234"
+    // interpreter looks at the first value "number" and the second value "string". One of those values has to be converted so they can be added together. It changes the value of 123 to a string and then appends.
+
+// = operator: is always for assignment, example: assigning a variable
+// == operator: compares values, returns a boolean
+// === operator: compares values AND data type, returns a boolean
