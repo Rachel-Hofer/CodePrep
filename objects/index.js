e8cc8af83ad5e05fa79a3a580ca4e91ecf767c4a
Rachel Hofer <rhofer3@gmail.com>
2019-01-12 20:57:04 -0500

    objects

---
+++ objects/index.js
@@ -0,0 +1,7 @@
+// Objects
+// JS is an object-oriented language BUT it is not a class-based language
+
+// object: 
+  // simplest example: var myObj = {};
+
+// object: a collection of data (properties) and functionality (methods) 
